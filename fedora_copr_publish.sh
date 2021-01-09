2415efd4ba739f13dd2975dd635c1d8681100959
Marcin Szydelski <marcin@szydelscy.pl>
2021-01-09 16:18:34 +0100

    repair sort

--- fedora_copr_publish.sh
+++ fedora_copr_publish.sh
@@ -18,7 +18,7 @@ version_in_changelog=$(grep -E "system76-driver \([[:digit:]]+\.[[:digit:]]+\.[[
 _tmp=${version_in_changelog%%)*}
 version=${_tmp##*\(}
 
-_tmp=$(git tag --list system76-driver-"$version"-'*' | sort -r -n | head -1)
+_tmp=$(git tag --list system76-driver-"$version"-'*' | sort -n -t '-' -k 4 -r | head -1)
 release=${_tmp##*-}
 
 if [ "z$release" == "z" ]; then
