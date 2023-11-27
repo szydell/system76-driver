ff6a8ccf2fb29c25cfe42a711a499cffbecf8b37
Marcin Szydelski <marcin@szydelscy.pl>
2023-11-27 22:03:01 +0100

    shellcheck

--- fedora_copr_publish.sh
+++ fedora_copr_publish.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 #
 # Parse debian/changelog and set package version same as on PopOS! and Ubuntu
-# 
+#
 # ----------------------------------------------------------------------------
 # 2021-01-09 Marcin Szydelski
 #		init
 
 # config
-export outdir="$(pwd)/.rpkg-build"
+outdir="$(pwd)/.rpkg-build"
+export outdir
 
 # verification
-[ -f debian/changelog ] || { echo "No debian/changelog found."; exit 1; }
+[[ -f debian/changelog ]] || {
+	echo "No debian/changelog found."
+	exit 1
+}
 
 # fetch upstream
 git fetch upstream
@@ -18,8 +22,7 @@ git fetch upstream
 git checkout master
 git merge upstream/master -m "fetch upstream" --log
 
-
-[ -d ${outdir} ] && rm -rf ${outdir}
+[[ -d ${outdir} ]] && rm -rf "${outdir}"
 
 version_in_changelog=$(grep -E "system76-driver \([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" debian/changelog | head -1)
 _tmp=${version_in_changelog%%)*}
@@ -32,7 +35,7 @@ release=${_tmp##*-}
 if [ "z$release" == "z" ]; then
 	release=1
 else
-	if ! [[ "$release" =~ ^[0-9]+$ ]]; then
+	if ! [[ $release =~ ^[0-9]+$ ]]; then
 		echo "Release should be a number"
 		exit 2
 	fi
@@ -47,20 +50,32 @@ git commit -m"bump Version to: $version-$release" system76-driver.spec.rpkg
 
 #test & build srpm
 mkdir "$outdir"
-rpkg local --outdir="$outdir" || { echo "rpkg local failed"; exit 4; }
+rpkg local --outdir="$outdir" || {
+	echo "rpkg local failed"
+	exit 4
+}
 
 # rpkg tag
-rpkg tag --version="$version"  --release="$release"
+rpkg tag --version="$version" --release="$release"
 
 srpm="$(ls .rpkg-build/system76-driver-*.src.rpm)"
 
 # publish / build oficially
 
-copr-cli build system76 "$srpm" || { echo "Copr build failed"; exit 5; }
+copr-cli build system76 "$srpm" || {
+	echo "Copr build failed"
+	exit 5
+}
 
 # store in repo
-git push || { echo "Git push failed"; exit 6; }
-git push --tags || { echo "Git push --tags failed"; exit 6; }
+git push || {
+	echo "Git push failed"
+	exit 6
+}
+git push --tags || {
+	echo "Git push --tags failed"
+	exit 6
+}
 
 # clear
 
